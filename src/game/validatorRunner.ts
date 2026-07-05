fbfcec4bed0d25704aca147c1ff5497f8eb51fc7
Ofek Levy <ofekxlevy@gmail.com>
2026-07-06 00:21:43 +0300

    validatorRunner

---
+++ src/game/validatorRunner.ts
@@ -0,0 +1,24 @@
+import type {ValidationResult} from './validationResult';
+
+export type Validator<T> = (value: T) => ValidationResult;
+
+function runValidatorsFromIndex<T>(
+    validators: Validator<T>[],
+    value: T,
+    index: number
+): ValidationResult {
+    if (index >= validators.length) {
+        return { isValid: true };
+    }
+    const result = validators[index](value);
+    return result.isValid
+        ? runValidatorsFromIndex(validators, value, index + 1)
+        : result;
+}
+
+export function runValidators<T>(
+    validators: Validator<T>[],
+    value: T
+): ValidationResult {
+    return runValidatorsFromIndex(validators, value, 0);
+}
