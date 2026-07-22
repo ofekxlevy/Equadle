44fa213409857679a7ba70dc55767b1bc2c87293
Ofek Levy <ofekxlevy@gmail.com>
2026-07-22 21:04:19 +0300

    imports

--- src/game/basicValidators.ts
+++ src/game/basicValidators.ts
@@ -1,5 +1,5 @@
-import { ALLOWED_TOKENS, DIGIT_TOKENS, EQUATION_LENGTH } from './types';
-import type { DigitToken, Token } from './types';
+import { ALLOWED_TOKENS, EQUATION_LENGTH } from './types';
+import type { Token } from './types';
 import { isDigit } from './types';
 import { makeValidResult, makeInvalidResult} from './result';
 import type { ValidationResult } from './result';
